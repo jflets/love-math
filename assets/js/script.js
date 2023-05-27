19a71507670c414eded228ba858c5521073ed537
jflets <jfletorides02@gmail.com>
2023-05-27 13:29:02 +0100

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit");
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 /**
  * The main game "loop", called when rhw script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestions(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 function checkAnswers() {
     
@@ -32,8 +41,10 @@ function incrementScore() {
 function incrementWrongAnswers() {
     
 }
-function displayAdditionQuestions() {
-    
+function displayAdditionQuestions(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 function displaySubtractQuestions() {
     

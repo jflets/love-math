02a05b33061c72861d37bcf0bf3d24154d072c4e
jflets <jfletorides02@gmail.com>
2023-05-27 12:53:44 +0100

    Setup structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame() {
+    
+}
+function checkAnswers() {
+    
+}
+function calculateCorrectAnswers() {
+    
+}
+function incrementScore() {
+    
+}
+function incrementWrongAnswers() {
+    
+}
+function displayAdditionQuestions() {
+    
+}
+function displaySubtractQuestions() {
+    
+}
+function displayMultiplyQuestions() {
+    
+}
